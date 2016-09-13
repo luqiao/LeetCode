3a7d076c38d44c64d40fa8055add5bd4d822495c
luqiao <1812773694@qq.com>
2016-09-13 16:49:54 +0800

    2016/09/13

--- LEETCODE/src/addBinary67/Solution.java
+++ LEETCODE/src/addBinary67/Solution.java
@@ -22,10 +22,10 @@ public class Solution {
     		int inta=chara[lena-1-i]-'0';
     		int intb=charb[lenb-1-i]-'0';
     		sb.append(inta^intb^carry);
-    		carry=(inta&intb)|(inta^intb&carry);
+    		carry=(inta&intb)|((inta^intb)&carry);
     	}
     	//add the longer length numbers
-    	for (int i=lenb-lenb-1;i>=0;i--){
+    	for (int i=lenb-lena-1;i>=0;i--){
     		int intb=charb[i]-'0';
     		sb.append(intb^carry);
     		carry=intb&carry;
@@ -41,6 +41,8 @@ public class Solution {
 		// TODO Auto-generated method stub
 		String a="10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101";
 		String b="110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011";
+		//String a="101";
+		//String b="10111";
 		System.out.println(addBinary(a,b));
 		//110111101100010011000101110110100000011101000101011001000011011000001100011110011010010011000000000
 		
--- LEETCODE/src/myAtoi8/Solution.java
+++ LEETCODE/src/myAtoi8/Solution.java
@@ -9,10 +9,84 @@ The signature of the C++ function had been updated. If you still see your functi
 
 public class Solution {
     public static int myAtoi(String str) {
-    	return 1;
+    	StringBuilder sb=new StringBuilder();
+    	int flag=0;
+    	if(str.length()==0)
+    		return 0;
+    	else{
+    		for(int i=0;i<str.length();i++){
+    			if(str.charAt(i)<'0'||str.charAt(i)>'9')
+    				if(str.charAt(i)=='0'||str.charAt(i)==' ')
+    					if (sb.length()==0)
+    						continue;
+    					else
+    						break;
+    				else if (str.charAt(i)=='-')
+    					if (flag==0){
+    						flag=-1;
+    						sb.append('0');
+    					}
+    					else
+    						break;
+    				else if (str.charAt(i)=='+')
+    					if (flag==0){
+    						flag=1;
+    						sb.append('0');
+    					}
+    					else
+    						break;
+    				else
+    					break;
+    			else
+    				sb.append(str.charAt(i));		
+    			}		
+    	}
+    	if(sb.length()==0)
+    		return 0;
+    	else if(sb.length()>12)
+    		str=sb.substring(sb.length()-12);
+    	else
+    		str=sb.toString();
+    	if (flag==-1)
+			str="-"+str;
+    	long result=Long.parseLong(str);
+    	System.out.println(result);
+    	if (result>2147483647)
+    		return 2147483647;
+    	else if (result<-2147483648)
+    		return -2147483648;
+    	else
+    		return (int)result;
         
     }
     public static void main(String[] args){
-    	
+    	String str1="";//0
+    	String str2="+";//0
+    	String str3="              +45555";//45555
+    	String str4="              -45555";//-45555
+    	String str5="2147483647";//2147483647
+    	String str6="2147483648";//2147483647
+    	String str7="+-2";//0
+    	String str8="-+2";//0
+    	String str9="++2";//0
+    	String str10="-2147483648";//-2147483648
+    	String str11="   - 321";//0
+    	String str12="      -11919730356x";//-2147483648
+    	String str13="1234567890123456789012345678901234567890";//2147483647
+    	System.out.println(myAtoi(str1));
+    	System.out.println(myAtoi(str2));
+    	System.out.println(myAtoi(str3));
+    	System.out.println(myAtoi(str4));
+    	System.out.println(myAtoi(str5));
+    	System.out.println(myAtoi(str6));
+    	System.out.println(myAtoi(str7));
+    	System.out.println(myAtoi(str8));
+    	System.out.println(myAtoi(str9));
+    	System.out.println(myAtoi(str10));
+    	System.out.println(myAtoi(str11));
+    	System.out.println(myAtoi(str12));
+    	System.out.println(myAtoi(str13));
+
+
     }
 }
